daa5decd6883556110d8f2ca6fd9a6167491fe67
Brandon Woods <woods.brandon.m@gmail.com>
2023-04-07 16:25:13 -0700

    move database table setup code to separate file in lib

--- server/controllers.js
+++ server/controllers.js
@@ -3,9 +3,8 @@ const model = require('./models.js');
 module.exports = {
 
   getQuestions: (req, res) => {
-    // console.log('query: ', req.query);
     model.getQuestionsById(req.query.product_id)
     .then(data => res.send(data))
-    .catch(err => console.log('err:', err))
+    .catch(err => res.send(err))
   }
 }
