eb4b0e4f8b469ea6ec38d8e9eaf4d92ce6896673
Brandon Woods <woods.brandon.m@gmail.com>
2023-04-08 10:52:37 -0700

    add object listing answers for each question;

--- server/controllers.js
+++ server/controllers.js
@@ -4,13 +4,36 @@ module.exports = {
 
   getQuestions: (req, res) => {
     model.getQuestionsById(req.query.product_id)
-    .then(data => {
-      let result = {
-        product_id: req.query.product_id,
-        results: data,
+    .then(async questions => {
+      for (let question of questions) {
+        question.answers = {};
+        let answerList = await model.getAnswersById(question.id);
+        for (let answer of answerList.rows) {
+          question.answers[answer.id] = answer;
+        }
       }
-      res.send(result);
+      return questions;
     })
+    // .then(questions => {
+
+    // })
+    .then(questions => res.send(questions))
     .catch(err => res.send(err))
-  }
+  },
+
+  postQuestion: (req, res) => {
+    // model query
+    // something
+  },
+
+  getAnswers: (req, res) => {
+
+    model.getAnswersById(req.params.question_id)
+    .then(data => {
+      // console.log('data', data);
+      res.send(data);
+    })
+    .catch(err => res.send(err));
+  },
+
 }
--- server/routes.js
+++ server/routes.js
@@ -2,30 +2,37 @@ const controller = require('./controllers.js');
 const express = require('express');
 const router = express.Router()
 
-// /qa/questions
-  // GET
-  // POST
-
 router.get('/', (req, res) => {
   res.send('Welcome to the BlueGillAPI!')
 })
 
+// /qa/questions
+  // GET
+  // POST
 router.get('/qa/questions', controller.getQuestions);
+router.post('/qa/questions', controller.postQuestion);
 
-
-module.exports = router;
 // /qa/questions/:question_id/helpful
   // PUT
+router.put('/qa/questions/:question_id/helpful', );
 
 // /qa/questions/:question_id/report
   // PUT
+router.put('/qa/questions/:question_id/report');
 
 // /qa/questions/:question_id/answers
   // GET
   // POST
+router.get('/qa/questions/:question_id/answers', controller.getAnswers);
+router.post('/qa/questions/:question_id/answers');
+
 
 // /qa/answers/:answer_id/helpful
   // PUT
+router.put('/qa/answers/:answer_id/helpful')
 
 // /qa/answers/:answer_id/report
   // PUT
+router.put('/qa/answers/:answer_id/report')
+
+module.exports = router;
