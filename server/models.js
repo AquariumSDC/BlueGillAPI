c35d564e0145051517aa3ae9caee4945718bdfee
Brandon Woods <woods.brandon.m@gmail.com>
2023-04-11 16:56:10 -0700

    load testing complete

--- server/models.js
+++ server/models.js
@@ -3,32 +3,23 @@ const db = require('./db.js');
 const models = {
 
   getQuestionsById: async (product_id) => {
-    // let query =  `SELECT id, question_body, question_date, asker_name, question_helpfulness, reported
-    // FROM questions WHERE product_id = ${product_id} AND reported = 0`;
-    let query = `SELECT jsonb_agg(jsonb_build_object('id', id, 'question_body',question_body, 'question_date', question_date, 'asker_name', asker_name, 'question_helpfulness', question_helpfulness, 'reported', reported))
+    let query =  `SELECT id, question_body, question_date, asker_name, question_helpfulness, reported
     FROM questions WHERE product_id = ${product_id} AND reported = 0`;
     let res = await db.query(query)
-    // console.log('res', res.rows[0].json_agg)
-    // return res.rows;
-    return res.rows[0].json_agg;
+    return res.rows;
   },
 
   getAnswersById: async (question_id) => {
-    // let query = `SELECT json_build_object('id', id, 'body', body, 'date', date, 'answerer_name', answerer_name, 'helpfulness', helpfulness,'reported', reported)
-    // FROM answers WHERE id_questions = ${question_id} AND reported = 0`;
     let query = `SELECT id, body, date, answerer_name, helpfulness, reported
     FROM answers WHERE id_questions = ${question_id} AND reported = 0`;
     let res = await db.query(query)
-    // console.log('answers', res.rows)
     return res.rows;
   },
 
   getPhotosById: async (answer_id) => {
-    // let query = `SELECT id, url FROM photos WHERE id_answers = ${answer_id}`;
-    let query = `SELECT jsonb_agg(jsonb_build_object('id', id,'url', url)) FROM photos WHERE id_answers = ${answer_id}`;
+    let query = `SELECT id, url FROM photos WHERE id_answers = ${answer_id}`;
     let res = await db.query(query)
-    console.log('photos: ', res.rows[0].json_agg);
-    return res.rows[0].json_agg;
+    return res.rows;
   },
 
   postQuestion: async ({body, name, email, product_id, date}) => {
--- tests/loadTest.js
+++ tests/loadTest.js
@@ -2,12 +2,10 @@ const http = require('k6/http');
 const { check, sleep } = require('k6');
 
 export const options = {
-  // vus: 1000,
-  // duration: '60s',
   scenarios: {
     QnA: {
       executor: 'constant-arrival-rate',
-      rate: 1000,
+      rate: 400,
       timeUnit: '1s',
       duration: '30s',
       preAllocatedVUs: 1000,
@@ -16,17 +14,7 @@ export const options = {
   }
 };
 
-// const url = 'http://localhost:3000';
-
 export default () => {
-
   const QRes = http.get('http://localhost:3000/qa/questions');
   check(QRes, { 'status was 200': (r) => r.status == 200 });
-  // sleep(1);
-
 }
-
-
-  // const ARes = http.get('http://localhost:3000/qa/questions/:question_id/answers');
-  // check(ARes, { 'status was 200': (r) => r.status == 200 });
-  // sleep(1);
